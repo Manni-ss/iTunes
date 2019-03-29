3c31271d9a0243be90dae4256573476f7223603c
enrique <enriquespicersaave@students.berkeley.net>
2019-03-29 13:48:25 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+
+    $("#button").click(function() {
+
+        var artist = $("#artist").val();
+
+        $.ajax({
+            url: "http://itunes.apple.com/search?term=" + artist,
+            dataType: "jsonp",
+            success: function (answer){
+                myCallBack(answer);
+                print(answer);
+        }
+        });
+
+    })
+
+});
+
+function myCallBack(myData){
+    console.log(myData);
+}
+function print(songs){
+    $("#table").empty();
+    var list = "";
+    songs.resultCount = $("#select").val();
+    for (var i = 0; i < songs.resultCount; i++) {
+        list += '<tr><td>';
+        list += '<span class="rank">' + (i+1) + '.  </span>';
+        list += "<img src='" + songs.results[i].artworkUrl100 + "'>";
+        list += "<td>" + "<a href = 'detail.html?artist=" + songs.results[i].artistName + "&song=" + i + "'>"+ songs.results[i].trackName +"</a>";
+        list += '</td><td>';
+        list += '</td><td>';
+        list += "<audio controls='true' src=" + songs.results[i].previewUrl + " id='audio' type='audio/m4a'></audio>";
+        list += '</td></tr>';
+
+    }
+    $("#table").append(list);
+}
+
---
+++ script2.js
@@ -0,0 +1,52 @@
+$.ajax({
+        url: "http://itunes.apple.com/search?term=" + getQueryParameter("artist"),
+        dataType: "jsonp",
+        success: function (answer){
+            myCallBack(answer);
+            }
+
+
+});
+
+
+function myCallBack(songs){
+    console.log(songs);
+    var x = songs.results[getQueryParameter("song")].trackTimeMillis/1000;
+    var min = Math.floor(x/60);
+    var sec = Math.floor(x%60);
+    var date = new Date(songs.results[getQueryParameter("song")].releaseDate);
+    var explicit = songs.results[getQueryParameter("song")].trackExplicitness;
+    var price = songs.results[getQueryParameter("song")].trackPrice;
+    var genre = songs.results[getQueryParameter("song")].primaryGenreName;
+    $("#newTable").empty();
+    var details = "";
+    details += '<tr><td>';
+    details += "<img src='" + songs.results[getQueryParameter("song")].artworkUrl100 + "'>";
+    details += '<br><td>' + songs.results[getQueryParameter("song")].trackName + '</a></td>';
+    details += '</td><td>';
+    details += '</td><br><td>';
+    details += "<audio controls='true' src=" + songs.results[getQueryParameter("song")].previewUrl + " id='audio' type='audio/m4a'></audio>";
+    details += '</td></tr>';
+    details += "<tr><td>";
+    if(sec<10){
+        details += "Time " + min + ":0" + sec;
+    } else {
+        details += "Time " + min + ":" + sec
+    }
+    details += "</td><td>" + "Release Date: " + date.getMonth() + "/" + 27 + "/" + date.getFullYear() + " </td>";
+    details += "<td>" +   "Language: " + explicit +  "</td>";
+    details += "<td>        Price: $" + price + "</td>";
+    details += "<td>" + "       Genre: " + genre + "</td>";
+    details += "</tr>";
+    $("#newTable").append(details);
+
+}
+function getQueryParameter(name) {
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i=0;i<vars.length;i++) {
+        var pair = vars[i].split("=");
+        if(pair[0] == name){return pair[1];}
+    }
+    return false;
+}
